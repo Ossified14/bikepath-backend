c00ab1b318e33a3d8d666cbebbd4fd27ef9fc146
sshli <ossifiedcorridor@gmail.com>
2026-06-11 22:14:15 +0700

    fix: add forced CORS headers in index.php

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 /**
+ * Forced CORS for Production
+ */
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Content-Length, Accept-Encoding, Authorization, X-Requested-With, Origin, Accept");
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header("HTTP/1.1 200 OK");
+    exit;
+}
+
+/**
  * CodeIgniter
  *
  * An open source application development framework for PHP
